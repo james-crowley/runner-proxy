c1f57fbcfa825497e80bed883827a2bc45d79ac0
james-crowley <jim.crowley@circleci.com>
2022-04-28 18:18:40 -0400

    Pushing updates to the proxy and readme

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # CircleCI Runner Proxy
 
-Proxies the the request from CircleCI Runner agent by setting `API URL` configuration parameter.
+Proxies the the request from CircleCI Runner agent by setting `API URL` configuration parameter. The proxy will hijack the requests for downloading launch agent and task agent.
+Once hijacked, they will point to the binaries located in the `binaries/`. 
 
-To use the proxy, run the docker container and set the `api.url` parameter in your runner's config to IP/DNS of where your proxy is hosted.
+## Running Runner Proxy
+To run the Runner Proxy issue the following command:
 
-Make sure to set the `PROXY_SERVER` environment variable inside the docker container to your IP/DNS. 
+
+```bash
+docker run --env PROXY_SERVER=http://runner.circleci-demo-app.com -d -p 80:8080 jimcrowley/runner-proxy:latest 
+```
+
+Make sure to set the `PROXY_SERVER` environment variable to point to your own IP/DNS.
+
+
+
+## Runner Config
+When using the Runner Proxy its recommended to run with the following config:
+
+```yaml
+api:
+  auth_token: AUTH_TOKEN
+  url: http://runner.circleci-demo-app.com
+
+logging:
+  file: /opt/circleci/runner.log
+
+runner:
+  name: RUNNER_NAME
+  command_prefix: ["sudo", "-niHu", "circleci", "--"]
+  working_directory: /opt/circleci/workdir/%s
+  cleanup_working_directory: true
+  disable_auto_update: true
+```
+
+Make sure to set the `url` value to your IP/DNS of where your proxy is running.
--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import hashlib
 
 
+# Setting up Basic Logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
@@ -37,7 +38,7 @@ def _proxy(*args, **kwargs):
         cookies=request.cookies,
         allow_redirects=False)
 
-    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    excluded_headers = []
     headers = [(name, value) for (name, value) in resp.raw.headers.items()
                if name.lower() not in excluded_headers]
 
@@ -54,51 +55,52 @@ def runner_api():
     logging.info("Printing Headers we got from servers: ")
     logging.info(request.headers)
 
-    if request.method == "GET":
-        logging.info("GET Request Issued!")
 
-        # Grab JSON Body from the request
-        content_type = request.headers.get('Content-Type')
-        if (content_type == 'application/json'):
-            request_json = request.json
-            logging.info("Printing JSON Body we got from servers: ")
-            logging.info(request_json)
+    # Grab JSON Body from the request
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        request_json = request.json
+        logging.info("Printing JSON Body we got from servers: ")
+        logging.info(request_json)
 
-        # If Architecture is s390x or ppc64le, then hijack the request and redirect the download to our webserver
-        if request_json["arch"] == "s390x" or request_json["arch"] == "ppc64le":
-            # Template for response
-            url = "http://{proxy_server}/circleci-agent/{version}/{os}/{arch}/circleci-agent".format(proxy_server=os.getenv('PROXY_SERVER'), version=request_json["version"], os=request_json["os"], arch=request_json["arch"])
-            path_to_download = "binaries/{arch}/circleci-launch-agent".format(arch=request_json["arch"])
-            
-            template = {
-                "url": url,
-                "checksum": sha256sum(path_to_download),
-                "version": request_json["version"]
-            }
+    # If Architecture is s390x or ppc64le, then hijack the request and redirect the download to our webserver
+    if request_json["arch"] == "s390x" or request_json["arch"] == "ppc64le":
+        # Template for response
+        url = "http://{proxy_server}/circleci-agent/{version}/{os}/{arch}/circleci-agent".format(proxy_server=os.getenv('PROXY_SERVER'), version=request_json["version"], os=request_json["os"], arch=request_json["arch"])
+        path_to_download = "binaries/{arch}/circleci-agent".format(arch=request_json["arch"])
+        
+        # Since we are not building every version of task agent for s390x/ppc64le, just say the version is the version requested and dynamically gerenate checksum
+        template = {
+            "url": url,
+            "checksum": sha256sum(path_to_download),
+            "version": request_json["version"]
+        }
 
-            # Return and render JSON object
-            return jsonify(template), 200
+        # Return and render JSON object
+        return jsonify(template), 200
 
-        # Else proxy the request back to the official Runner API
-        else:
-            resp = requests.request(
-                method=request.method,
-                url=request.url.replace(request.host_url, 'https://runner.circleci.com/'),
-                headers={key: value for (key, value) in request.headers if key != 'Host'},
-                data=request.get_data(),
-                cookies=request.cookies,
-                allow_redirects=False)
+    # Else proxy the request back to the official Runner API
+    else:
+        resp = requests.request(
+            method=request.method,
+            url=request.url.replace(request.host_url, 'https://runner.circleci.com/'),
+            headers={key: value for (key, value) in request.headers if key != 'Host'},
+            data=request.get_data(),
+            cookies=request.cookies,
+            allow_redirects=False)
 
-            excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-            headers = [(name, value) for (name, value) in resp.raw.headers.items()
-                    if name.lower() not in excluded_headers]
+        excluded_headers = []
+        headers = [(name, value) for (name, value) in resp.raw.headers.items()
+                if name.lower() not in excluded_headers]
 
-            response = Response(resp.content, resp.status_code, headers)
-            return response
+        response = Response(resp.content, resp.status_code, headers)
+        return response
 
-# Route to Build/Task Agent
+
+# Route to Build/Task Agent Download
 @app.route('/circleci-agent/<version>/<os>/<arch>/circleci-agent', methods=["GET"])
 def build_agent_download(version, os, arch):
+    # We do not need to redirect request for other architectures since we only accept/give out URLs for s390x/ppc64le
     path_to_download = "binaries/{arch}/circleci-agent".format(arch=arch)
     return send_file(path_to_download, attachment_filename='circleci-agent')
 
@@ -106,9 +108,11 @@ def build_agent_download(version, os, arch):
 # Route to the Launch Agent or Proxy the Connect to Official Launch Agent Download
 @app.route('/circleci-launch-agent/<version>/<os>/<arch>/circleci-launch-agent', methods=["GET"])
 def launch_agent_download(version, os, arch):
+    # If someone requests the launch agent for s390x/ppc64le
     if arch == "s390x" or arch == "ppc64le":
         path_to_download = "binaries/{arch}/circleci-launch-agent".format(arch=arch)
         return send_file(path_to_download, attachment_filename='circleci-launch-agent')
+    # If not s390x/ppc64le, then redirect request back to official S3 Bucket
     else:
         resp = requests.request(
             method=request.method,
@@ -118,7 +122,7 @@ def launch_agent_download(version, os, arch):
             cookies=request.cookies,
             allow_redirects=False)
 
-        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        excluded_headers = []
         headers = [(name, value) for (name, value) in resp.raw.headers.items()
                 if name.lower() not in excluded_headers]
 
@@ -126,17 +130,26 @@ def launch_agent_download(version, os, arch):
         return response
 
 
-# Catch All Route that will Proxy any Requests
-@app.route('/', defaults={'path': ''})
-@app.route("/<string:path>")
-@app.route('/<path:path>')
+# Catch All Route that will proxy/redirect all requests back to official runner API
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'DELETE', 'PUT', 'PATCH'])
+@app.route("/<string:path>", methods=['GET', 'POST', 'DELETE', 'PUT', 'PATCH'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'DELETE', 'PUT', 'PATCH'])
 def catch_all(path):
     # Print out User Agent
     logging.info("User Agent Information: " + request.user_agent.string)
 
+    # Print Out Headers
     logging.info("Printing Headers we got from servers: ")
     logging.info(request.headers)
 
+    # Grab JSON Body from the request
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        request_json = request.json
+        logging.info("Printing JSON Body we got from servers: ")
+        logging.info(request_json)
+
+    # Replace host with official runner API URL
     resp = requests.request(
         method=request.method,
         url=request.url.replace(request.host_url, 'https://runner.circleci.com/'),
@@ -145,7 +158,7 @@ def catch_all(path):
         cookies=request.cookies,
         allow_redirects=False)
 
-    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    excluded_headers = []
     headers = [(name, value) for (name, value) in resp.raw.headers.items()
                if name.lower() not in excluded_headers]
 
